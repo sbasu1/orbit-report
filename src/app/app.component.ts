7cd1a5592563d4f884aa18999ec4cc83fab745ce
sbasu1 <ghoshsreya@gmail.com>
2019-11-01 00:38:00 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   sourceList: Satellite[];
   title = 'orbit-report';
+  displayList: Satellite[];
   constructor(){
   //   this.sourceList = [
   //     new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
@@ -20,6 +21,7 @@ export class AppComponent {
 
    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
    this.sourceList = [];
+   this.displayList = [];
    window.fetch(satellitesUrl).then(function(response) {
       response.json().then(function(data) {
 
@@ -28,9 +30,23 @@ export class AppComponent {
           this.sourceList.push(new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational));
          }
        
-
+         this.displayList = this.sourceList.slice(0);
       }.bind(this));
    }.bind(this));
 
   }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+ }
 }
